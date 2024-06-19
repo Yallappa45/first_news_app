cd81b89d4af6c6d714b4f4e2689bc59b6f98daa1
CameraOpen <yallappachalawadi45@gmail.com>
2024-06-19 11:52:10 +0530

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import ArticleList from './components/ArticleList';
+import CategoryFilter from './components/CategoryFilter';
+import Pagination from './components/Pagination';
 
-function App() {
+const API_KEY = '16607d4443594f1aa540e05bc3d8700e';
+const PAGE_SIZE = 5;
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [categories, setCategories] = useState(['Technology', 'Business', 'Sports', 'Health', 'Science', 'Entertainment']);
+  const [selectedCategory, setSelectedCategory] = useState('technology');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+
+  useEffect(() => {
+    fetchArticles();
+  }, [selectedCategory, currentPage]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines`, {
+        params: {
+          category: selectedCategory,
+          page: currentPage,
+          pageSize: PAGE_SIZE,
+          apiKey: API_KEY
+        }
+      });
+      setArticles(response.data.articles);
+      setTotalResults(response.data.totalResults);
+    } catch (error) {
+      console.error('Error fetching the news articles:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>News Articles</h1>
+      <CategoryFilter categories={categories} selectedCategory={selectedCategory} onSelectCategory={setSelectedCategory} />
+      <ArticleList articles={articles} />
+      <Pagination currentPage={currentPage} totalResults={totalResults} pageSize={PAGE_SIZE} onPageChange={setCurrentPage} />
     </div>
   );
-}
+};
 
 export default App;
